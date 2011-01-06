d8e05d5ff0d3f3f280b0678a39d9564b55f06036
christmo99 <christmo99@50d329f0-3116-36fc-1c78-68f260d219d4>
2011-01-06 17:28:37 +0000

    Cambio en la obtención de archivo de configuración

--- PantallasCliente/src/utilitarios/Utilitarios.java
+++ PantallasCliente/src/utilitarios/Utilitarios.java
@@ -6,6 +6,7 @@ package utilitarios;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -123,7 +124,12 @@ public class Utilitarios {
             if (jarDir != null && jarDir.isDirectory()) {
                 File propFile = new File(jarDir, arc);
                 prop = new Properties();
-                prop.load(new BufferedReader(new FileReader(propFile.getAbsoluteFile())));
+                try {
+                    prop.load(new BufferedReader(new FileReader(propFile.getAbsoluteFile())));
+                } catch (FileNotFoundException fx) {
+                    System.err.println("No se encuentra el archivo: " + fx.getMessage());
+                    System.exit(0);
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(Utilitarios.class.getName()).log(Level.SEVERE, null, ex);
