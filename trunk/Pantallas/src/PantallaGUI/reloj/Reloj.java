bc3d68bc02865eab460cf8723fdd32eb550fc465
christmo99 <christmo99@50d329f0-3116-36fc-1c78-68f260d219d4>
2011-10-03 21:39:28 +0000

    Boton de desbloqueo de pantallas, igualar la hora de la interfaz en vez de la hora del sistema a las 2 pantallas...

---
+++ trunk/Pantallas/src/PantallaGUI/reloj/Reloj.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PantallaGUI.reloj;
+
+import PantallaGUI.PrincipalGUI;
+import java.security.Principal;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class Reloj {
+
+    SimpleDateFormat sdfHora = new SimpleDateFormat("HH:mm:ss");
+
+    public Reloj() {
+        Cronometro c = new Cronometro();
+        Timer timer = new Timer();
+        timer.schedule(c, 0, 1000);
+    }
+
+    class Cronometro extends TimerTask {
+
+        String hora = "";
+
+        public void run() {
+            GregorianCalendar c = new GregorianCalendar();
+
+            hora = sdfHora.format(c.getTime());
+            //System.out.println(c.getTimeInMillis() + " = " + hora);
+
+            if (hora.equals("12:59:10")) {
+                igualarHoraPantallas(hora);
+            }
+        }
+
+        private void igualarHoraPantallas(String hora) {
+            PrincipalGUI.igualarHoras(hora, 1);
+            PrincipalGUI.igualarHoras(hora, 2);
+        }
+    }
+}
